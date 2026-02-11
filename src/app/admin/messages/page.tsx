d9ec793ec46ab371829519dab380a50a9b7bb10d
Ntnaza <masadepanku1712@gmail.com>
2026-02-11 09:07:56 +0700

    build

--- src/app/admin/messages/page.tsx
+++ src/app/admin/messages/page.tsx
@@ -118,7 +118,6 @@ export default function AdminMessages() {
               </div>
             )}
           </div>
-        )}
       </div>
 
       {/* Modal Detail Pesan */}
--- src/app/admin/services/page.tsx
+++ src/app/admin/services/page.tsx
@@ -118,7 +118,6 @@ export default function AdminServices() {
               </div>
             )}
           </div>
-        )}
       </div>
 
       <ServiceForm 
